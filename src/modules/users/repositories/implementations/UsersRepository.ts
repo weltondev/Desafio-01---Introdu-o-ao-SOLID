3f0113cbae6e6ad106722d41bd59f1441b10581b
WeltonDev <welton.developer95@gmail.com>
2023-06-16 20:50:51 -0300

    CreateUser and ListUser

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,43 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const userId = this.users.find((user) => user.id === id);
+
+    return userId;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const userName = this.users.find((user) => user.email === email);
+
+    return userName;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    Object.assign(receivedUser, {
+      admin: true,
+      updated_at: new Date(),
+    });
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    const users = this.listAllUsersUseCase.execute({
+      user_id,
+    });
+
+    return response.send({ users });
   }
 }
 
